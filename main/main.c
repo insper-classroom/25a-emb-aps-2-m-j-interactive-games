09bf9c9851fdc6b246d497f2e977e1d6b33a4a16
Iamiran1 <vinicius.c.miranda@icloud.com>
2025-04-24 17:03:44 -0300

    CORREÇÃO DE TIRO

--- main/main.c
+++ main/main.c
@@ -72,92 +72,15 @@ void fire_task(void *p) {
             xQueueSend(xQueueFire, &shot, 0);
             fire =0;
             gpio_put(PIN_VIB,1);
-            sleep_ms(500);
+            vTaskDelay(pdMS_TO_TICKS(200));
             gpio_put(PIN_VIB,0);
-
-        }
-        else{
-            shot = 0;
-            xQueueSend(xQueueFire, &shot, 0);
 
         }
 
         vTaskDelay(pdMS_TO_TICKS(200));
     }
 }
-const uint SEL_A_4051 = 13;
-const uint SEL_B_4051 = 12;
-const uint SEL_C_4051 = 11;
-const uint INH_4051   = 10;
-const int PIN_LEDS_ON = 27;
 
-void polling_adc_init(void) {
-    gpio_init(SEL_A_4051);
-    gpio_set_dir(SEL_A_4051, GPIO_OUT);
-
-    gpio_init(SEL_B_4051);
-    gpio_set_dir(SEL_B_4051, GPIO_OUT);
-
-    gpio_init(SEL_C_4051); 
-    gpio_set_dir(SEL_C_4051, GPIO_OUT);
-
-    gpio_init(INH_4051);         
-    gpio_set_dir(INH_4051, GPIO_OUT);
-    gpio_put(INH_4051, 1);       // começa desabilitado
-}
-
-void select_4051_channel(uint channel) {
-    gpio_put(SEL_A_4051, channel & 0x01);
-    gpio_put(SEL_B_4051, (channel >> 1) & 0x01);
-    gpio_put(SEL_C_4051, (channel >> 2) & 0x01);
-}
-
-
-void munition_show(int show_number){
-    stdio_init_all();
-
-    polling_adc_init();
-
-    gpio_init(PIN_LEDS_ON);    // GPIO27 = ADC1
-    gpio_set_dir(PIN_LEDS_ON, GPIO_OUT);
-
-    for (uint channel = 0; channel <= show_number; channel++) {
-        gpio_put(PIN_LEDS_ON,1);
-        // gpio_put(INH_4051, 1);               // desabilita 4051
-        select_4051_channel(channel);       // muda canal
-        sleep_ms(2);                        // tempo de setup
-        gpio_put(INH_4051, 0);               // habilita 4051
-        sleep_ms(2);                        // tempo de estabilização
-        printf("Canal %d: %d\n", channel, gpio_get(PIN_LEDS_ON));
-    }
-    printf("--------------------\n");    
-    vTaskDelay(pdMS_TO_TICKS(200));
-
-
-}
-void munition_task(void *p){
-    int shot_data;
-    int munition_counter = 8;
-    while (1) {
-        if (xQueueReceive(xQueueFire, &shot_data, portMAX_DELAY)) {
-            if(shot_data == 1){
-                send_uart_packet(2,1);
-                munition_counter --;
-                printf("munition: %d \n",munition_counter);
-                if(munition_counter == 0){
-                    munition_counter = 8;
-                }
-            }
-            
-            munition_show(munition_counter);
-
-
-        }
-
-
-    }
-
-}
 #define SAMPLE_PERIOD (0.01f) // replace this with actual sample period
 
 const int MPU_ADDRESS = 0x68;
@@ -338,6 +261,9 @@ void uart_task(void *p) {
                 }
             }
         }
+        if (xQueueReceive(xQueueFire, &shot_data, portMAX_DELAY)) {
+            send_uart_packet(2, 1);
+        }
 
     }
 }
@@ -347,7 +273,6 @@ int main() {
     xQueueFire = xQueueCreate(32, sizeof(int));
     xQueuePos = xQueueCreate(32, sizeof(data));
     xTaskCreate(fire_task, "Fire task", 4095, NULL, 1, NULL);
-    xTaskCreate(munition_task, "Munition task", 4095, NULL, 1, NULL);
     xTaskCreate(mpu6050_task, "mpu6050_Task 1", 8192, NULL, 1, NULL);
     xTaskCreate(uart_task, "uart_task", 4095, NULL, 1, NULL);
 
